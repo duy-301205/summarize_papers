516660329d609f95ec0b48a6641a2136d951fea0
Anh Duy <hduy9863@gmail.com>
2026-05-15 00:37:49 +0700

    Update

--- summarize_papers_fe/src/assets/components/Header.jsx
+++ summarize_papers_fe/src/assets/components/Header.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Search } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import NotificationDropdown from "./NotificationDropdown";
+import { getMe } from "../config/api";
 
 const Header = () => {
   const navigate = useNavigate();
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await getMe();
+        setUser(res.data.data);
+      } catch (error) {
+        console.error("Lỗi lấy thông tin user:", error);
+      }
+    };
+
+    fetchUser();
+  }, []);
 
   return (
     <header className="h-20 bg-white border-b border-slate-200 flex items-center justify-between px-8 shrink-0 sticky top-0 z-10 font-display">
@@ -24,21 +40,24 @@ const Header = () => {
       <div className="flex items-center gap-6">
         <NotificationDropdown />
         <div className="h-8 w-px bg-slate-200"></div>
+
         <div
           onClick={() => navigate("/settings")}
           className="flex items-center gap-3 pl-2 cursor-pointer hover:opacity-80 transition-all group"
         >
           <div className="text-right hidden sm:block">
             <p className="text-sm font-bold text-slate-900 leading-none group-hover:text-[#1111d4] transition-colors">
-              DuyHoang
+              {user?.username || "User"}
             </p>
+
             <p className="text-[10px] font-bold text-[#1111d4] uppercase mt-1 tracking-tighter">
-              VNU Researcher
+              {user?.institution || "Researcher"}
             </p>
           </div>
+
           <img
             className="size-10 rounded-full border-2 border-blue-100 shadow-sm object-cover group-hover:border-[#1111d4] transition-all"
-            src="https://i.pravatar.cc/150?u=duy"
+            src={`https://ui-avatars.com/api/?name=${user?.username || "User"}`}
             alt="Avatar"
           />
         </div>
